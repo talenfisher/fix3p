26af7accb5aa69d6aa1ece60cc613c17086cad17
Travis CI <travis@example.com>
2018-10-03 15:04:58 +0000

    Pages build 17

--- assets/js/uploader.js
+++ assets/js/uploader.js
@@ -1,6 +1,6 @@
 import jszip from "jszip";
 import ZipHolder from "./zipholder";
-import EditableFields from "./editable-fields.json";
+import Editor from "./editor";
 
 let parser = new DOMParser();
 class X3PException {};
@@ -52,113 +52,10 @@ export default class Uploader {
         }
 
         let manifest = await fix3p.ZipHolder.retrieve("main.xml");
-        manifest = parser.parseFromString(manifest, "application/xml");
+        let editor = new Editor();
 
-        switch(window.fix3p.mode) {
-            case "development": 
-                this.display(manifest.children[0]);
-                break;
-
-            default:
-                this.populate(manifest.children[0]);
-                break;
-        }
-        
-        document.querySelector("form").style.right = "100vw";
+        editor.display(parser.parseFromString(manifest, "application/xml"));
     }
-
-    /**
-     * Populate existing HTML inputs with values
-     * from the X3P file
-     * 
-     * @param {*} node 
-     */
-    populate(node) {
-        if(node.children.length === 0) {
-            let selector = pathArray2DTS(node.getPath());
-            let el = document.querySelector(selector + " input");
-            el.value = node.innerHTML;
-
-        } else {
-            for(let subchild of node.children) {
-                this.populate(subchild);
-            }
-        }
-    }
-
-    display(manifest) {
-        this.count = 0;
-        document.querySelector(".view main").innerHTML = "";
-        document.querySelector(".view nav").innerHTML = '<a href="#" class="tab"><i class="fas fa-download"></i> Download</a>';
-        this.displayIterator(manifest, document.querySelector(".view main"));
-    }
-
-    /**
-     * Dynamically populate the editor with what is in
-     * the X3P file.  (Used in development to generate 
-     * the "viewer" HTML tree in index.html)
-     * 
-     * @param {*} manifest 
-     * @param {*} target 
-     */
-    displayIterator(manifest, target) {
-        for(let child of manifest.children) {
-            let el = document.createEasy("div", {
-                attrs: { "data-tag": child.tagName } 
-            });
-            
-            if(child.children.length > 0) {
-                el = this.displayIterator(child, el);
-    
-                // record headings should be tabs instead
-                if(!child.tagName.match(/^Record/g)) {
-                    let heading = document.createEasy("h3", {
-                        props: { innerHTML: prettyPrint(child.tagName) }
-                    });
-                
-                    el.insertBefore(heading, el.children[0]);
-    
-                } else {
-                    let tab = document.createEasy("div", {
-                        props: { "innerHTML": prettyPrint(child.tagName) },
-                        attrs: { "data-target": child.tagName },
-                        classes: [ "tab" ]
-                    });
-
-                    target.parentElement.querySelector("nav").appendChild(tab);
-                }
-    
-            } else {
-                let label = document.createEasy("label", {
-                    props: {
-                        innerHTML: prettyPrint(child.tagName) + ":"
-                    },
-                    attrs: {
-                        for: "x3p$"+this.count
-                    }
-                });
-
-                let input = document.createEasy("input", {
-                    props: {
-                        type: "text",
-                        value: child.innerHTML,
-                        id: "x3p$"+this.count
-                    }
-                });    
-
-                if(!EditableFields.includes(this.count)) input.setAttribute("disabled", "disabled");
-                
-                el.appendChild(label);
-                el.appendChild(input);
-                this.count++;
-            }
-    
-            target.appendChild(el);
-        }
-    
-        return target;
-    }    
-
 }
 
 /**
