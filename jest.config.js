d255d2a733ff3152de5755b59e746c3c4f4e9d6e
Talen Fisher <tqfisher@iastate.edu>
2018-12-12 18:42:18 -0600

    move tsconfig to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  preset: './jest-preset.js',
-  testEnvironment: 'jest-environment-puppeteer',
-  globalSetup: "jest-environment-puppeteer/setup",
-  globalTeardown: "jest-environment-puppeteer/teardown"
+    preset: './jest-preset.js',
+    testEnvironment: 'jest-environment-puppeteer',
+    globalSetup: "jest-environment-puppeteer/setup",
+    globalTeardown: "jest-environment-puppeteer/teardown",
+    globals: {
+        "ts-jest": {
+            tsConfig: {
+                lib: ["es2015", "dom"]
+            }
+        }
+    }
 };
