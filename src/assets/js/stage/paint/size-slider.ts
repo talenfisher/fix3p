2ba9c8efba27adaa55b4d2dc316b35e9276edb62
Talen Fisher <tqfisher@iastate.edu>
2019-04-30 12:18:02 -0500

    only use whole number brush sizes

--- src/assets/js/stage/paint/size-slider.ts
+++ src/assets/js/stage/paint/size-slider.ts
@@ -45,7 +45,7 @@ export default class SizeSlider extends HTMLElement {
         if(!Session.started) return;
 
         let brush = Session.brush;
-        brush.size = this.value * this.maxBrushSize;
+        brush.size = Math.ceil(this.value * this.maxBrushSize);
         Logger.action(`brush size changed to ${brush.size}`, Session.filename);
     }
 }
