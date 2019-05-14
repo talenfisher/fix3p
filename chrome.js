7214bb659abfad5833f425bb2e726ef8ac2a3633
Travis CI <travis@example.com>
2019-05-14 16:09:52 +0000

    Pages build 278

--- chrome.js
+++ chrome.js
@@ -1,14 +1,48 @@
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
-    var displayURL;
 
-    if (/\.x3p$/i.test(details.url)) {
-        displayURL = chrome.runtime.getURL("index.html");
-        localStorage.setItem("openfile", details.url);
-        return { redirectUrl: displayURL };
-    }   
-}, {urls: ['file://*/*'], types:["main_frame", "sub_frame"]}, ['blocking', 'requestBody']);
+function getHeader(needle, haystack = []) {
+    for(let straw of haystack) {
+        if(straw.name.toLowerCase() === needle.toLowerCase()) {
+            return straw.value;
+        }
+    }
+}
 
-var redirect = () => chrome.tabs.create({ url: chrome.extension.getURL("index.html") });
+function redirect() {
+    chrome.tabs.create({ 
+        url: chrome.extension.getURL("index.html") 
+    });
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        if(/.x3p$/i.test(details.url)) {
+            localStorage.setItem("openfile", details.url);
+            return { redirectUrl: chrome.runtime.getURL("index.html") };
+        }
+    },
+    {
+        urls: ["file://*/*"],
+        types: ["main_frame"]
+    },
+    ["blocking"]
+);
+
+chrome.webRequest.onHeadersReceived.addListener(
+    function(details) {
+        var contentType = getHeader("content-type", details.responseHeaders);
+
+        if(details.frameId === 0 && contentType === "application/x-x3p") {
+            localStorage.setItem("openfile", details.url);
+            return { redirectUrl: chrome.runtime.getURL("index.html") };
+        }   
+    }, 
+    {
+        urls: ['<all_urls>'],
+        types: ['main_frame']
+    }, 
+    ['blocking', 'responseHeaders', 'extraHeaders']
+);
+
 chrome.browserAction.onClicked.addListener(redirect);
 
 chrome.extension.isAllowedFileSchemeAccess(allowed => {
@@ -29,7 +63,7 @@ chrome.extension.isAllowedFileSchemeAccess(allowed => {
         setInterval(interval, 1000);
 
         chrome.browserAction.onClicked.removeListener(redirect);
-        chrome.browserAction.onClicked.addListener(() => {
+        chrome.browserAction.onClicked.addListener(() => {  
             chrome.tabs.create({ url: chrome.extension.getURL("setup.html") })
         });
     } else chrome.browserAction.setBadgeText({ text: '' });
@@ -40,5 +74,3 @@ chrome.runtime.onInstalled.addListener(() => {
         if(!allowed) chrome.tabs.create({ url: chrome.extension.getURL("setup.html") });
     });
 });
-
-
