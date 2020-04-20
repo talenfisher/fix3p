6ab4e63cad7bc2ef7b2b35f314a04c00bc25d860
Talen Fisher <talen.fisher@cythral.com>
2020-04-20 10:32:02 -0500

    FIX3P-18 Paint Mode: Show Annotation Colors

--- tests/unit/util.test.ts
+++ tests/unit/util.test.ts
@@ -1,4 +1,6 @@
-import { getHeaderPart } from "../../src/assets/js/util";
+import { getHeaderPart, getX3pAnnotationColors } from "../../src/assets/js/util";
+import Color from "@talenfisher/color";
+import X3P from "x3p.js";
 
 describe("util", () => {
     describe("getHeaderPart", () => {
@@ -12,6 +14,29 @@ describe("util", () => {
             let header = 'attachment; filename="test.jpg"';
             let name = getHeaderPart(header, "name");
             expect(name).toBeNull();
+        });
+    });
+
+    describe("getX3pAnnotationColors", () => {
+        it("Should return an array of colors including annotation colors and read colors", () => {
+            const annotations = { ["#ff0000"]: 'red value', ["#0000ff"]: 'blue value' };
+            const colors = [ new Color("#00ff00") ];
+            const mask = { annotations, colors };
+            const x3p = { mask } as any;
+
+            const actual = getX3pAnnotationColors(x3p);
+            expect(actual).toEqual(["#ff0000", "#0000ff", "#00ff00"]);
+        });
+        
+        it("Should exclude the background color", () => {
+            const annotations = { ["#ff0000"]: 'red value', ["#0000ff"]: 'blue value' };
+            const colors = [ new Color("#00ff00") ];
+            const background = "#ff0000";
+            const mask = { annotations, colors, color: background };
+            const x3p = { mask } as any;
+
+            const actual = getX3pAnnotationColors(x3p);
+            expect(actual).toEqual(["#0000ff", "#00ff00"]);
         });
     });
 });
